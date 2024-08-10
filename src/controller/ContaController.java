0fbb5fffccfe6604cec4e004ac77deacf0b13f8b
Guilherme <guilherme94siqueira@gmail.com>
2024-08-09 21:51:30 -0300

    arquivos mode/controller

---
+++ src/controller/ContaController.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class ContaController {
+     public class ContaController {
+     
+            private ArrayList<Conta> listaContas = new ArrayList<conta> ();
+
+     }
+}
