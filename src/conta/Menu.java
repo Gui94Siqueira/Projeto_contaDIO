99aae117fb2a13f83cc78e6f333324c3a499411a
Guilherme <guilherme94siqueira@gmail.com>
2024-08-12 21:04:41 -0300

    iniciando menu

---
+++ src/conta/Menu.java
@@ -0,0 +1,16 @@
+package conta;
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import conta.controller.ContaController;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
+
+
+public class Menu {
+
+    public static Scanner leia = new Scanner(System.in);
+    
+}
